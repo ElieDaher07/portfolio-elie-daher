9af0fece031baa99224cd491730e7b9f9a851a5f
ElieDaher07 <143806470+ElieDaher07@users.noreply.github.com>
2025-10-15 19:11:46 -0400

    Mobile finalisé, 95% Desktop presque fini
    HTML/CSS mobile stabilisés
    
    Ajout de swiper bundle pour remplacer la version ChatGPT de ma carte
    Ajout une petite animation de logo swiper en Javascript
    Quelques ajustements de padding et font-size etc...
    
    Ajout de display grid 2x3 pour la version ipad/desktop
    
    Media queries iPad/desktop
    
    TODO : barre logos + responsive, bg image de la bannière desktop, ajouter de l'animation GSAP, filtres/popup Vue

--- src/scripts.js
+++ src/scripts.js
@@ -6,3 +6,19 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const logosSwiper = new Swiper('.logos-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 6,
+    autoplay: {
+        delay: 1200,
+        disableOnInteraction: false
+    }
+});
+
+const projetsSwiper = new Swiper('.projets-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    centeredSlides: true,
+    loop: false
+});
